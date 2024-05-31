6fcfeea48dbc09783d7787dd80b9a8c95f2732eb
Sarim Hasam <sarimhasanplace123@gmail.com>
2024-05-31 20:19:49 +0500

    do some rss updates

--- src/pages/rss.xl.js
+++ src/pages/rss.xl.js
@@ -1,11 +1,22 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blog = await getCollection('blog');
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    trailingSlash: false,
+    items: await pagesGlobToRssItems(import.meta.glob('./blog/*.{md,mdx}')),
   });
 }
+// export async function GET(context) {
+//   return rss({
+//     title: 'Astro Learner | Blog',
+//     description: 'My journey learning Astro',
+//     site: context.site,
+//     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//     customData: `<language>en-us</language>`,
+//   });
+// }
